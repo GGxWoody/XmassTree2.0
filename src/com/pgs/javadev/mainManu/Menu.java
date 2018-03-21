c5e05b7c16a06368cfddc8d4eb5dcad29bccab5b
GGxWoody <przemekp97@interia.eu>
2018-03-21 12:24:43 +0100

    Small changes #2

--- src/com/pgs/javadev/mainManu/Menu.java
+++ src/com/pgs/javadev/mainManu/Menu.java
@@ -1,9 +1,11 @@
 package com.pgs.javadev.mainManu;
 
 import com.pgs.javadev.interfaces.TreeDraw;
+import com.pgs.javadev.messages.MASSAGES;
 
 public class Menu {
     public static void StartProgram(){
+        System.out.println(MASSAGES.WELCOME_MASSAGE);
         TreeDraw finalTree = DecisionMaker.TreeToDraw(DirectionScanner.ScanDirection(),SizeScanner.TreeSize());
         finalTree.drawTree();
     }
--- src/com/pgs/javadev/messages/MASSAGES.java
+++ src/com/pgs/javadev/messages/MASSAGES.java
@@ -4,7 +4,7 @@ public final class MASSAGES {
     public static final String WELCOME_MASSAGE = "Witam w programie rysujacym choinke";
     public static final String ERROR_WRONG_STRING = "Zly kierunek drzewa";
     public static final String ERROR_WRONG_SIZE = "Zla wielkosc drzewa(Zbyt duza lub ujemna)";
-    public static final String ERROR_NOT_NUMBER = "Podan NUMER";
+    public static final String ERROR_NOT_NUMBER = "PODANA WARTOSC TO NIE LICZBA";
     public static final String TREE_DIRECTION = "Podaj kierunek drzewa(UP, DOWN, LEFT, RIGHT): ";
-    public static final String TREE_SIZE = "Podaj rozmiar drzewa(LICZBA dodatnia): ";
+    public static final String TREE_SIZE = "Podaj rozmiar drzewa(LICZBA dodatnia i mnijsza od 100): ";
 }
